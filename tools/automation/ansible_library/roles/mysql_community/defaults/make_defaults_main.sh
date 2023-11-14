fb5eafa13b06adbd9ccfaa3b6a8c3cdc6432e324
Mark N <markresume@menprojects.com>
2023-11-14 07:45:18 -0800

    auto

---
+++ tools/automation/ansible_library/roles/mysql_community/defaults/make_defaults_main.sh
@@ -0,0 +1,8 @@
+
+
+cat main.yml_template                  > main.yml
+cat mysql_source.yml_template          >> main.yml
+cat mysql_server_defaults.yml_template >> main.yml
+cat mysql_client_defaults.yml_template >> main.yml
+
+

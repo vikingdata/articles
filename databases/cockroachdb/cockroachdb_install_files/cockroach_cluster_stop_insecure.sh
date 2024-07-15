40c17300a4b2d8e003f817c664354fcff5619457
Mark N <markresume@menprojects.com>
2024-07-14 19:52:39 -0700

    auto

--- databases/cockroachdb/cockroachdb_install_files/cockroach_cluster_stop_insecure.sh
+++ databases/cockroachdb/cockroachdb_install_files/cockroach_cluster_stop_insecure.sh
@@ -6,6 +6,7 @@ cockroach node drain 1  --port=26257 --insecure
 if [ -f node1.pid ]; then
   echo "killing node 1"
   kill `cat node1.pid`
+  rm node1.pid
 fi
 sleep 5
 
@@ -14,20 +15,18 @@ cockroach node drain 2  --port=26258 --insecure
 if [ -f node1.pid ]; then
   echo "killing node 2"
   kill `cat node2.pid`
+  rm node2.pid
 fi
 sleep 5
 
 echo shutting down node3
-cockroach node drain 3  --port=26259 --insecure
+# cockroach node drain 3  --port=26259 --insecure
 if [ -f node3.pid ]; then
   echo "killing node 3"
   kill `cat node3.pid`
-fi
-sleep 5
-sleep 10
-if [ -f node3.pid ]; then
-  echo "hard kill node 3"
+  sleep 10
   kill -9 `cat node3.pid`
+  rm node3.pid
 fi
 
 echo '
@@ -39,7 +38,7 @@ sleep 5
 ' > list_cockroach_db_processes
 chmod 755 list_cockroach_db_processes
   
-watch -n 5  ./list_cockroach_db_processes 
+timeout 16 watch -n 3  ./list_cockroach_db_processes 
 if [ -f node3.pid ]; do
   echo "hard kill node 3"
   kill -9 `cat node3.pid`

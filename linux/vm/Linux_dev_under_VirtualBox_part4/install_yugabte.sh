4cc152a79161eb4600d98c7b1b2bd50d2e36871b
Mark N <markresume@menprojects.com>
2025-03-22 18:10:44 -0400

    auto

--- linux/vm/Linux_dev_under_VirtualBox_part4/install_yugabte.sh
+++ linux/vm/Linux_dev_under_VirtualBox_part4/install_yugabte.sh
@@ -1,16 +1,21 @@
 
+### Execute as root
+if [ ! $USER = 'root' ];
+   echo "Not root user, aborting"
+fi
+
 mkdir -p /root/software_install
 cd /root/software_install
 
 rm -rf /usr/local/yugabyte-2024.2.2.1
-rm -f /usr/local/yugabyte-2024_server
+rm -f /usr/local/yugabyte-2024_serverz
 
 
 wget https://software.yugabyte.com/releases/2024.2.2.1/yugabyte-2024.2.2.1-b6-linux-x86_64.tar.gz
 tar xvfz yugabyte-2024.2.2.1-b6-linux-x86_64.tar.gz
-mv yugabyte-2024.2.2.1 /usr/local
+sudo mv yugabyte-2024.2.2.1 /usr/local
 cd /usr/local
-ln -s yugabyte-2024.2.2.1 yugabyte-2024_server
+sudo ln -s yugabyte-2024.2.2.1 yugabyte-2024_server
 cd /usr/local/yugabyte-2024_server
 echo "PATH=/usr/local/yugabyte-2024_server/bin:\$PATH" >> ~/.bashrc
 source ~/.bashrc

4f0b3dd9f9924c54abb61cea867ee4563a338c1d
Mark N <markresume@menprojects.com>
2025-05-06 18:29:53 -0400

    auto

--- databases/yugabyte/yugabyte_general_files/restart_yugabyte.sh
+++ databases/yugabyte/yugabyte_general_files/restart_yugabyte.sh
@@ -4,7 +4,7 @@ export ybconfig=`locate yugabyte.config`
 export base_dir=`jq .base_dir $ybconfig | sed -e 's/\"//g'`
 export data_dir=`jq .data_dir $ybconfig | sed -e 's/\"//g'`
 #  export myip=`ifconfig  | grep "inet " | grep -v 127 | sed -e 's/  */ /g' | cut -d ' ' -f3`
-#     export myip=`hostname -I`
+#  export myip=`hostname -I`
 myip=`jq .advertise_address $ybconfig | sed -e 's/"//g'`
 
 echo "
@@ -12,10 +12,6 @@ advertising ip : $myip
 base_dir       : $base_dir
 data_dir       : $data_dir
 "
-
-ysqlsh -h $myip -c "select yb_servers();" 2>/dev/null > /dev/null
-yactive="$?"
-
 ysqlsh -h $myip -c "select yb_servers();" 2>/dev/null > /dev/null
 yactive="$?"
 
@@ -24,7 +20,18 @@ if [ "$yactive" = 0 ]; then
 else
   echo "starting yugabyte"
   yugabyted start --config $ybconfig
-fi
 
-ysqlsh -h $myip -c "select yb_servers();" 2>/dev/null > /dev/null
-yactive="$?"
+  1ysqlsh -h $myip -c "select yb_servers();" 2>/dev/null > /dev/null
+  yactive="$?"
+
+  if [ "$yactive" = 0 ]; then
+     ysqlsh -h $myip -c "select yb_servers();"
+  else
+      echo "Unable to connect to yugabyte"
+      ps -e -o pid,user,comm | egrep "postgres|yb-|yuga"
+      echo "Unable to connect to yugabyte, check the processes above"
+      echo "You might want to run
+        ps -aux | egrep 'postgres|yb-|yuga' | grep -v grep        "
+
+  fi
+fi
